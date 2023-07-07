f2d144ebb64be569cdf85fb08c99bcfa81069e2e
Ritesh Navadiya <riteshnavadiya111@gmail.com>
2023-07-07 10:39:19 +0530

    API fech data ui set

--- Maltipal_Task/src/api_call.js
+++ Maltipal_Task/src/api_call.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {AxiosGetRequest, AxiosPostRequest} from './axios';
 import axios from 'axios';
 
@@ -57,14 +57,45 @@ const APIcallAxios = () => {
 
   return (
     <View>
-      <Text>Welcom API Call With Axios</Text>
       <ScrollView>
-        <Text>{data && JSON.stringify(data)}</Text>
+        {/* <Text>{data && JSON.stringify(data)}</Text> */}
+        <FlatList
+          data={data}
+          renderItem={({item, index}) => {
+            return (
+              <View style={styles.dataBody}>
+                {/* <Text style={styles.contentHedar}>ID :</Text>
+                <Text>{item.id}</Text> */}
+                <Text style={styles.contentHedar}>{item.title} </Text>
+                <Text style={styles.contentBody}>{item.title}</Text>
+                {/* <Text style={styles.contentHedar}>Content :</Text> */}
+                <Text style={styles.contentBody}>{item.body}</Text>
+              </View>
+            );
+          }}
+        />
       </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  dataBody: {
+    flex: 1,
+    backgroundColor: '#e6e6e6',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  contentHedar: {
+    fontSize: 20,
+    backgroundColor: '#ffe6ff',
+    padding: 5,
+    borderRadius: 10,
+  },
+  contentBody: {
+    marginTop: 5,
+  },
+});
 
 export default APIcallAxios;
